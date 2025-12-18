af798e7b0fdd2078b1a11e02ecba3a1ff57d7da0
nismygaffoor <nismygaffoor5@gmail.com>
2025-12-18 22:13:44 +0530

    Update AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Bus;
+use App\Observers\BusObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register the BusObserver to automatically create seats when a bus is created
+        Bus::observe(BusObserver::class);
     }
 }
